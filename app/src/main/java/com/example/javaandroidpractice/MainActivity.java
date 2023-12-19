abeda9e505ab25019ac4eca39312fd3b116285a8
Tim <nsrv.tim@gmail.com>
2023-12-20 01:42:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/javaandroidpractice/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.javaandroidpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        ConstraintLayout buttonLayout = findViewById(R.id.Log_in_with_facebook_btn);
+        buttonLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
